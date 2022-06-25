48915e8516648dad6e02ac5560be6404a5c6e3e6
Inger-Merete Bruhn Cliquet <Inger-Merete.Bruhn@LEGO.com>
2022-06-25 10:46:09 +0200

    merging Sofiane and IM

--- server/models/place.js
+++ server/models/place.js
@@ -3,35 +3,33 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema(
-    {
-        
-        position: {
-            type: {
-              type: String,
-              default: 'Point'
-            },
-            coordinates: [Number]
-        },
-        creator: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-          },
-          description: {
-            type: String,
-            maxLength: 5000,
-            trim: true
-          },
-          pictures: [
-            {
-              type: String
-            }
-          ]
-        },
-    
+  {
+    position: {
+      type: {
+        type: String,
+        default: 'Point'
+      },
+      coordinates: [Number]
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    description: {
+      type: String,
+      maxLength: 5000,
+      trim: true
+    },
+    pictures: [
+      {
+        type: String
+      }
+    ]
+  },
 
-    { timestamps: true }
-  );
+  { timestamps: true }
+);
 
 const Place = mongoose.model('Place', schema);
 
